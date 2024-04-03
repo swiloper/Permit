cf99d375430214cbbdd912fe1e4fa5e134f7c5b4
swiloper <myronishyn.ihor@icloud.com>
2024-04-03 13:05:49 +0300

    Initial Commit

---
+++ Permit/PermitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PermitApp.swift
+//  Permit
+//
+//  Created by Ihor Myronishyn on 03.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct PermitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
